fc3a4130d70be3194dc42f9094b0349930f716ea
linnovel <luislinnovel@gmail.com>
2025-05-09 19:34:38 -0400

    proyect-completed

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+
+
+export const connectDb = async () => {
+
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB Connected : ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error ${error.message}`)
+        process.exit(1) // 1 signifca que hubo un error, 0 significa exito
+    }
+}
